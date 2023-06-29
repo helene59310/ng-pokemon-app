24d613fddf605b70ecf6a0873cbe724159726f15
Hélène Vasseur <heloise.59@hotmail.fr>
2023-06-29 15:28:06 +0200

    connexion serveur distant (api) et suppression de pokemon et méthode addpokemon dans le pokemonService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { InMemoryDataService } from './in-memory-data.service';
+
 
 
 
@@ -17,6 +22,8 @@ import { PokemonModule } from './pokemon/pokemon.module';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation:false }),
     PokemonModule,
     AppRoutingModule
   ],
--- src/app/pokemon/pokemon-form/pokemon-form.component.ts
+++ src/app/pokemon/pokemon-form/pokemon-form.component.ts
@@ -50,7 +50,8 @@ export class PokemonFormComponent implements OnInit {
   }
 
   onSubmit(){
-    console.log ('submit form !')
-    this.router.navigate(['/pokemon', this.pokemon.id]);
+    this.pokemonService.updatePokemon(this.pokemon)
+    .subscribe((pokemon) =>{ this.router.navigate(['/pokemon', this.pokemon.id])
+  });
   }
 }
