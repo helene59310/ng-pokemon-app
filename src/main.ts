74c7ec388a9826f7c1bfd97d0c84ddf01e14b832
Hélène Vasseur <heloise.59@hotmail.fr>
2023-07-16 18:35:48 +0200

    suppression des ng modules

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,36 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { AuthGuard } from './app/auth.guard';
+import { provideRouter, Routes } from '@angular/router';
+import { InMemoryDataService } from './app/in-memory-data.service';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+
+const routes: Routes = [
+    { path: '', redirectTo: 'pokemons', pathMatch: 'full' },
+    { path: '',
+        canActivate: [AuthGuard],
+        loadChildren: () => import("./app/pokemon/pokemon.routes") },
+    { path: 'login',
+        title: 'Login',
+        loadComponent: () => import("./app/login/login.component").then(module => module.LoginComponent) },
+    { path: '**',
+        title: 'Page not found',
+        loadComponent: () => import("./app/page-not-found/page-not-found.component").then(module => module.PageNotFoundComponent) }
+];
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+
+bootstrapApplication(AppComponent, {
+    providers: [
+        provideHttpClient(withInterceptorsFromDi()),
+        importProvidersFrom(BrowserModule, FormsModule, HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false })),
+        provideRouter(routes)
+    ]
+})
+.catch(err => console.error(err));
